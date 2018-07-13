748617b38db7c2601a9364bd68dc91104ca63607
wosayttn <wosayttn@gmail.com>
2018-07-13 18:31:49 +0800

    Support RNG hal implementation.

--- platform/mcu/numicro_m48x/hal/hal_rng.c
+++ platform/mcu/numicro_m48x/hal/hal_rng.c
@@ -22,6 +22,40 @@
 #include <hal/hal.h>
 #include "board.h"
 
+#define DEF_MISSION_TIMEOUT		100
+
+kmutex_t 	port_mutex;
+ksem_t 		done_sem;
+static int  g_prng_inited = 0;
+
+void CRYPTO_IRQHandler()
+{
+  if (PRNG_GET_INT_FLAG(CRPT))
+	{
+		krhino_sem_give (&done_sem);
+		PRNG_CLR_INT_FLAG(CRPT);
+	}
+}
+
+/**
+  * @brief  Read the PRNG key.
+  * @param[in]   crpt        Reference to Crypto module.
+  * @param[out]  u32RandKey  The key buffer to store newly generated PRNG key.
+  * @return None
+  */
+void PRNG_Recv(CRPT_T *crpt, uint32_t u32RandKey[], int expect_size)
+{
+    uint32_t  i, wcnt;
+
+    wcnt = (((crpt->PRNG_CTL & CRPT_PRNG_CTL_KEYSZ_Msk) >> CRPT_PRNG_CTL_KEYSZ_Pos) + 1U) * 2U;
+
+		wcnt = ( wcnt > expect_size ) ? expect_size: wcnt;
+	
+    for (i = 0U; i < wcnt; i++)
+        u32RandKey[i] = crpt->PRNG_KEY[i];
+
+    crpt->PRNG_CTL &= ~CRPT_PRNG_CTL_SEEDRLD_Msk;
+}
 
 /**
  * Fill in a memory buffer with random data
@@ -35,5 +69,71 @@
  */
 int32_t hal_random_num_read(random_dev_t random, void *buf, int32_t bytes)
 {
+	int offset=0;
+	int mission_len=0;
+	uint8_t* pbuf =  (uint8_t*)buf;
+	
+	if ( !buf || (bytes==0 ) )
+		return HAL_ERROR;
+		
+	// Initialize
+	if ( !g_prng_inited )
+	{
+		/* Unlock register lock protect */
+		SYS_UnlockReg();                   
+
+    /* Enable CRYPTO module clock */
+    CLK_EnableModuleClock(CRPT_MODULE);
+		
+		NVIC_EnableIRQ(CRPT_IRQn);
+		PRNG_ENABLE_INT(CRPT);
+
+		if ( krhino_sem_create(&done_sem, "uartSend", 0) != RHINO_SUCCESS )
+			goto exit_hal_random_num_read;
+
+		if ( krhino_mutex_create(&port_mutex, "uartMutex") != RHINO_SUCCESS )
+			goto exit_hal_random_num_read;
+		
+		g_prng_inited = 1;
+	}
+
+  /* Wait for Lock */
+  while ( krhino_mutex_lock(&port_mutex, 0xFFFFFFFF) != RHINO_SUCCESS );
+
+	while ( bytes > 0 )
+	{
+		/* start PRNG with seed 0x55 */
+		PRNG_Open(CRPT, PRNG_KEY_SIZE_256, 1, (unsigned int)aos_now_ms());
+		PRNG_Start(CRPT);
+		
+		if ( krhino_sem_take(&done_sem, DEF_MISSION_TIMEOUT) != RHINO_SUCCESS )
+			break;
+
+		mission_len = ( bytes >= 32 ) ? 32: bytes;
+
+		memset ( (void*) &pbuf[offset], 0, mission_len );
+		PRNG_Recv( CRPT, &pbuf[offset], mission_len );
+
+		bytes -= mission_len;
+		offset += mission_len;
+
+	} //while
+	
+  /* Unlock */
+  krhino_mutex_unlock(&port_mutex);
+		
 	return HAL_OK;
+
+exit_hal_random_num_read:
+
+	krhino_sem_del(&done_sem);
+	
+	krhino_mutex_del(&port_mutex);
+
+	// Finalize
+	PRNG_DISABLE_INT(CRPT);
+	NVIC_DisableIRQ(CRPT_IRQn);
+	g_prng_inited = 0;
+	
+	return HAL_ERROR;
 }
